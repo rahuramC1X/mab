530fccd1b1c04c2b6b95d3cd5e238bf3cfd4bfc7
rahuramC1X <rahuramdars@gmail.com>
2024-03-13 15:51:17 +0530

    roulette wheel popup

--- src/component/Wheel.tsx
+++ src/component/Wheel.tsx
@@ -1,3 +1,4 @@
+import { Dialog } from 'primereact/dialog';
 import React, { useEffect, useState } from 'react';
 import './MyRouletteComponent.css';
 
@@ -46,12 +47,14 @@ const MyRouletteComponent: React.FC = () => {
   const [wheels, setWheels] = useState<WheelData[]>([]);
   const [messages, setMessages] = useState<string[]>([]);
   const [spinCounts, setSpinCounts] = useState<number[]>(Array.from({ length: 10 }, () => 0)); // Array to track spin counts
-
+  const [clickedWheel, setClickedWheel] = useState<number | null>(null); // Track which wheel has been clicked
+  const [imageData, setImageData] = useState<string |null>(null)
+  const [showPopup, setShowPopup] = useState<boolean>(false)
 
   // Function to generate divisions with random numbers and weighted probabilities
   const generateDivisionsWithProbabilities = (probabilities: number[]): Division[] => {
     const divisions: Division[] = [];
-    const colors = ['#FF5733', '#FFC300', '#DAF7A6', '#C70039', '#581845', '#900C3F', '#FF5733', '#FFC300', '#DAF7A6', '#C70039'];
+    const colors = ['#F5F5DC', '#F4A460']; // Black and grey colors for divisions
   
     // Create an array of objects containing both number and probability
     const data: { number: number, probability: number }[] = [];
@@ -113,7 +116,8 @@ const handleSpinClick = (wheelIndex: number) => {
     // Wheel data not available yet, return early
     return;
   }
-  
+
+  setClickedWheel(wheelIndex); // Set the clicked wheel index  
   const newSpinCounts = [...spinCounts]; // Create a copy of the spin counts array
   newSpinCounts[wheelIndex] += 1; // Increment spin count for the clicked wheel
   setSpinCounts(newSpinCounts);
@@ -137,6 +141,9 @@ const handleSpinClick = (wheelIndex: number) => {
     return wheel;
   });
   console.log(newWheels)
+  setTimeout(() => {
+    setClickedWheel(null);
+  }, 500);
   setWheels(newWheels);
 };
 
@@ -164,14 +171,43 @@ const handleSpinClick = (wheelIndex: number) => {
     
   }, []);
 
+  const handleClosePopup = () => {
+    setShowPopup(false);
+    setImageData(null);
+  };
+
+  
+
 
   socket.onmessage = (event) => {
     const message = event.data;
     console.log('Received message from server:', message);
+    if(message === "start")
+      {setShowPopup(false)}
     if(message === "end" || message === "start"){
        totalSpinCount = 0
        const zero_array = new Array(10).fill(0)
        setSpinCounts(zero_array)
+    }
+    else 
+      if (message.startsWith('Image')) {
+        console.log("entered")
+        // Handle the image message
+        const jsonPart = message.substring('Image'.length);
+        try {
+          const parsedMessage = JSON.parse(jsonPart); // Parse the JSON message
+          if (parsedMessage && parsedMessage.image) {
+            // Extract the base64Image from the parsed message
+            const base64Image = parsedMessage.image;
+    
+            // Display the image using an <img> element
+            // Assuming you have a state variable to track the received image data
+            setImageData(base64Image); // Update the state with the image data
+            setShowPopup(true); // Show
+          }
+        } catch (error) {
+          console.error('Error parsing WebSocket message:', error);
+        }
     }
     else {
       const wheelIdToSpin = parseInt(message) - 1
@@ -199,36 +235,57 @@ const handleSpinClick = (wheelIndex: number) => {
   
   return (
     <div className="roulette-container">
-      
+      <h1 className="roulette-heading">Multi Armed Roulette Wheel</h1>
+      <div className='wheels-container'>
       {wheels.map((wheel, index) => (
-        <div key={index} className="wheel-container">
+        <div key={index} className="wheel-bg" >
+          <div key={index} className="wheel-container" onClick={() => handleSpinClick(index)}  >
+          
           <svg className={`wheel ${wheel.mustSpin ? 'spin' : ''}`} viewBox="0 0 300 300">
-            <circle cx="150" cy="150" r="140" fill="none" stroke="black" strokeWidth="2" />
+            <circle cx="150" cy="150" r="140" fill="#ffd700" stroke='white' strokeWidth="15" />
             {wheel.divisions.map((division, idx) => (
               <g key={idx}>
                 <path d={`M 150,150 L ${150 + 140 * Math.cos((idx / wheel.divisions.length) * Math.PI * 2)},${150 + 140 * Math.sin((idx / wheel.divisions.length) * Math.PI * 2)} A140,140 0 ${(1) % 10 === 0 ? '1' : '0'} 1 ${150 + 140 * Math.cos(((idx + 1) / wheel.divisions.length) * Math.PI * 2)},${150 + 140 * Math.sin(((idx + 1) / wheel.divisions.length) * Math.PI * 2)} Z`} fill={division.color} />
-
-                <text x={150 + 100 * Math.cos(((idx + 0.5) / 10) * Math.PI * 2)} y={150 + 100 * Math.sin(((idx + 0.5) / 10) * Math.PI * 2)} textAnchor="middle" dominantBaseline="middle" fill="white">{division.number}</text>
+                <text x={150 + 100 * Math.cos(((idx + 0.5) / 10) * Math.PI * 2)} y={150 + 100 * Math.sin(((idx + 0.5) / 10) * Math.PI * 2)} textAnchor="middle" dominantBaseline="middle" fill="black" fontWeight="bold">₹ {(division.number * 100)}</text>
               </g>
             ))}
             {wheel.prizeNumber !== null && (
               <line
-              x1="150"
-              y1="150"
-              x2={150 + 55 * Math.cos(((wheel.prizeNumber + 0.5) / 10) * Math.PI * 2)} // Adjusted length to half (from 110 to 55)
-              y2={150 + 55 * Math.sin(((wheel.prizeNumber + 0.5) / 10) * Math.PI * 2)} // Adjusted length to half (from 110 to 55)
-              stroke="black"
-              strokeWidth="6" // Adjusted strokeWidth for better proportion
-              strokeLinecap="round"
-            />
+                x1="150"
+                y1="150"
+                x2={150 + 55 * Math.cos(((wheel.prizeNumber + 0.5) / 10) * Math.PI * 2)}
+                y2={150 + 55 * Math.sin(((wheel.prizeNumber + 0.5) / 10) * Math.PI * 2)}
+                stroke="red"
+                strokeWidth="6"
+                strokeLinecap="round"
+              />
             )}
+            {wheel.prizeNumber !== null && (
+              <g className={`result-animation ${wheel.prizeNumber != null ? 'animate-result' : ''}`}>
+                <text x="150" y="150" textAnchor="middle" dominantBaseline="middle" className="funky-font" style={{ fill: 'purple', fontSize: '42px' }}>
+                 ₹ {wheel.divisions[wheel.prizeNumber].number * 100}
+                </text>
+              </g>
+            )}
+             
+
           </svg>
-          <div className="result">{wheel.prizeNumber != null && wheel.divisions[wheel.prizeNumber].number}</div>
-          <button onClick={() => {totalSpinCount += 1 
-            handleSpinClick(index)}} disabled={wheel.mustSpin}>SPIN</button>
+
+          
+          
+           
+          </div>
+          {clickedWheel === index && (
+              <div className={`hammer ${clickedWheel === index ? 'clicked' : ''}`}></div>
+            )}
         </div>
       ))}
-
+      </div>
+      {imageData && (
+         <Dialog header="Image Popup" visible={showPopup} onHide={handleClosePopup}>
+           <img src={`data:image/jpeg;base64,${imageData}`} alt="Received Image" />
+       </Dialog>
+      )}
     </div>
   );
 };
